7cd883552805d690401fc2c7854394618fe59992
seanmbowen <sean.bowen@fullstackit.ca>
2022-05-26 19:56:34 -0400

    ## Commit notes
    - Added data model
    - Closes #2

---
+++ src/main/java/com/fsit/msscbeerservice/web/model/BeerDto.java
@@ -0,0 +1,30 @@
+package com.fsit.msscbeerservice.web.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+/**
+ * Created by: seanbowen on 2022-05-26
+ * Architecting Solutions. Simply.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+    private UUID id;
+    private Integer version;
+    private OffsetDateTime createdDate;
+    private OffsetDateTime lastModifiedDate;
+    private String beerName;
+    private BeerStyle beerStyle;
+    private Long upc;
+    private BigDecimal price;
+    private Integer quantityOnHand;
+}
---
+++ src/main/java/com/fsit/msscbeerservice/web/model/BeerStyle.java
@@ -0,0 +1,9 @@
+package com.fsit.msscbeerservice.web.model;
+
+/**
+ * Created by: seanbowen on 2022-05-26
+ * Architecting Solutions. Simply.
+ */
+public enum BeerStyle {
+    LAGER, PISLSNER, ALE, STOUT, GOSE, IPA, PORTER
+}
